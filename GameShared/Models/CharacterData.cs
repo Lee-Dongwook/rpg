500954a3de7a270eafc5fa54766a3e1633e5d7ba
Lee-Dongwook <dlehddnr0713@gmail.com>
2026-09-05 21:17:02 +0900

    feat: setting

--- GameShared/Models/CharacterData.cs
+++ GameShared/Models/CharacterData.cs
@@ -3,10 +3,17 @@ namespace GameShared.Models
     public class CharacterData
     {
         public string Id { get; set; }
-        public string Name { get; set; }
         public int Hp { get; set; }
         public int MaxHp { get; set; }
         public int AttackPower { get; set; }
+
+        public CharacterData(string id, int maxHp, int attackPower)
+        {
+            Id = id;
+            MaxHp = maxHp;
+            Hp = maxHp; 
+            AttackPower = attackPower;
+        }
 
         public void ApplyDamage(int damage)
         {
